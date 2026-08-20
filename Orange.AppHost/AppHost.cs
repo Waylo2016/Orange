54ee7a5fe2f24c42ed6d2e3813730880dcbbe344
Waylo2016 <wellinkstijn@gmail.com>
2026-08-21 00:57:34 +0200

    initial commit pt 2 electric boogaloo

--- Orange.AppHost/AppHost.cs
+++ Orange.AppHost/AppHost.cs
@@ -1,5 +1,38 @@
-var builder = DistributedApplication.CreateBuilder(args);
+namespace Orange.AppHost;
 
-builder.AddProject<Projects.Orange_Api>("orange-api");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = DistributedApplication.CreateBuilder(args);
+        builder.AddDockerComposeEnvironment("env");
 
-builder.Build().Run();
+        var seq = builder.AddSeq("seq")
+            .ExcludeFromManifest()
+            .WithLifetime(ContainerLifetime.Persistent)
+            .WithEnvironment("ACCEPT_EULA", "Y");
+
+        var postgres = builder.AddPostgres("postgres")
+            .WithPgAdmin(pgAdmin => pgAdmin.WithHostPort(5050))
+            .WithDataVolume();
+        
+        var postgresdb = postgres.AddDatabase("postgresdb");
+
+        var api = builder.AddProject<Projects.Orange_Api>("api")
+            .WithHttpEndpoint(8080)
+            .WithReference(seq)
+            .WithReference(postgresdb)
+            .WaitFor(postgresdb);
+
+        var blazorApp = builder.AddBlazorWasmProject<Projects.Orange_Blazor>("blazor")
+            .WithReference(api)
+            .WithReference(seq);
+
+        var gateway = builder.AddBlazorGateway("gateway")
+            .WithExternalHttpEndpoints();
+
+        gateway.WithBlazorClientApp(blazorApp);
+        
+        builder.Build().Run();
+    }
+}
--- Orange.Blazor/Program.cs
+++ Orange.Blazor/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Orange.Api;
+using Orange.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
