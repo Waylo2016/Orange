5dd72715748878d2023dfa6afeb30e3a09d385ff
Waylo2016 <wellinkstijn@gmail.com>
2026-08-23 01:12:14 +0200

    refactored the code summary to be at the interface

--- Orange.Bot/Services/MessageWaiter.cs
+++ Orange.Bot/Services/MessageWaiter.cs
@@ -9,18 +9,6 @@ namespace Orange.Bot.Services;
 
 public class MessageWaiter(DiscordSocketClient client, ILogger<MessageWaiter> logger) : IMessageWaiter
 {
-    /// <summary>
-    /// waits for a message from a specific user in a specific channel, with a timeout and a cancellation token
-    /// </summary>
-    /// <param name="userId">the userId of the user to wait for</param>
-    /// <param name="channelId">the channelId of the user to wait for</param>
-    /// <param name="timeout">timeout preferred in seconds</param>
-    /// <param name="cancellationToken">cancellation token</param>
-    /// <returns>
-    /// The first message sent by <paramref name="userId"/> in the channel
-    /// identified by <paramref name="channelId"/> within the specified <paramref name="timeout"/>,
-    /// or <c>null</c> if no message is received within the timeout period or if the operation is canceled.
-    /// </returns>
     public async Task<SocketMessage?> WaitForMessageAsync(
         ulong userId, 
         ulong channelId, 
